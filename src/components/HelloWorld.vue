9f9aab67ed628c316c52034bc0958da75f6a4202
MrAnyx <1ptitigre@gmail.com>
2021-08-22 19:09:30 +0200

    Feat: Added naive-ui + xicons + vfonts + nodemon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,49 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >
-      Vetur
-    </a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&lt;script setup&gt;</code>)
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+   <div>
+      <h1>{{ msg }}</h1>
+      <n-space horizontal>
+         <n-button>Default</n-button>
+         <n-button type="primary">Primary</n-button>
+         <n-button type="info">Info</n-button>
+         <n-button type="success">Success</n-button>
+         <n-button type="warning">Warning</n-button>
+         <n-button type="error">Error</n-button>
+      </n-space>
+   </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from "vue";
+
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
-    }
-  },
-  setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+   name: "HelloWorld",
+   props: {
+      msg: {
+         type: String,
+         required: true,
+      },
+   },
+   setup: () => {
+      const count = ref(0);
+      return { count };
+   },
+});
 </script>
 
 <style scoped>
 a {
-  color: #42b983;
+   color: #42b983;
 }
 
 label {
-  margin: 0 0.5em;
-  font-weight: bold;
+   margin: 0 0.5em;
+   font-weight: bold;
 }
 
 code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+   background-color: #eee;
+   padding: 2px 4px;
+   border-radius: 4px;
+   color: #304455;
 }
 </style>
